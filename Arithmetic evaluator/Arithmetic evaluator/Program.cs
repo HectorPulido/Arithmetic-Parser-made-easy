7b0f5768f0d1053aaf61e5c61be11c25aab4e94e
Hector Pulido <Hectorandrespp@gmail.com>
2018-03-14 16:57:33 -0500

    First Commit

---
+++ Arithmetic evaluator/Arithmetic evaluator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_evaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            try
+            {
+                Console.WriteLine(Parser.Evaluator("5+(6/6)+(666+663)/214*4 + (666+663)"));
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
